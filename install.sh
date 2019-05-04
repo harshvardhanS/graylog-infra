fe33a0d526cfc8e8454b6621b40b0aa7dd4edb48
Harshvardhan Singh Asawat <asawat.harsh@gmail.com>
2019-05-04 17:49:20 +0530

    Update install.sh
    updated install.sh for nginx

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 #sudo apt-get install -y python
 #sudo apt-get install -y python-pip
 #sudo apt-get install -y ansible
-apt-get install -y nginx
-service start nginx
+sudo apt-get update
+sudo apt-get install -y nginx
 echo "<html><h1>Hello from harshvardhan</h2></html>" > /var/www/html/index.html
+sudo service restart nginx
